226fe273bb5717ed6113c3d49ca4cc79fc41c8b1
Peter Oberrauner <peter.oberrauner@codefluegel.com>
2019-03-22 15:31:28 +0100

    Add Initial Routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+// import Header from '../components/Header';
+// import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+// import AddExpensePage from '../components/AddExpensePage';
+// import EditExpensePage from '../components/EditExpensePage';
+// import HelpPage from '../components/HelpPage';
+// import NotFoundPage from '../components/NotFoundPage';
+import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
+
+// const App = () => (
+//   <BrowserRouter>
+//     <div>
+//       <Header />
+//       <Switch>
+//         <Route
+//           path="/"
+//           component={ExpenseDashboardPage}
+//           exact
+//         />
+//         <Route path="/create" component={AddExpensePage} />
+//         <Route path="/edit/:id" component={EditExpensePage} />
+//         <Route path="/help" component={HelpPage} />
+//         <Route component={NotFoundPage} />
+//       </Switch>
+//     </div>
+//   </BrowserRouter>
+// );
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
