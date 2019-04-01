915cd1d960fe46e788bba4b15b2f9cca38687c19
Peter Oberrauner <peter.oberrauner@codefluegel.com>
2019-04-01 18:19:40 +0200

    Format the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => (
   <Navbar bg="dark" variant="dark">
-    <LinkContainer to="/">
-      <Navbar.Brand>
-        <img
-          alt=""
-          src="/logo.ico"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />
-        {" AlmostBearded"}
-      </Navbar.Brand>
-    </LinkContainer>
-    <Nav className="ml-auto">
-      <LinkContainer to="/portfolio">
-        <Nav.Link>Portfolio</Nav.Link>
+    <Container>
+      <LinkContainer to="/">
+        <Navbar.Brand>
+          <img
+            alt=""
+            src="/logo.ico"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {" AlmostBearded"}
+        </Navbar.Brand>
       </LinkContainer>
-      <LinkContainer to="/contact">
-        <Nav.Link>Contact</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/about">
-        <Nav.Link>About</Nav.Link>
-      </LinkContainer>
-    </Nav>
+      <Nav className="ml-auto">
+        <LinkContainer to="/portfolio">
+          <Nav.Link>Portfolio</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/contact">
+          <Nav.Link>Contact</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/about">
+          <Nav.Link>About</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </Container>
   </Navbar>
 );
 
