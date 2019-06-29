56959d1f67535a426cbcf112e484890a2af2f9f4
Peter Oberrauner <thealmostbearded@gmail.com>
2019-06-30 00:53:48 +0200

    Add Multi Column List

---
+++ src/components/multi-column-list/MultiColumnList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const MultiColumnList = ({
+  xs = 1,
+  sm,
+  md,
+  lg,
+  xl,
+  items = [],
+  onRenderItem = item => {},
+  paddingY = 2
+}) => (
+  <Row>
+    {items.map((item, index) => (
+      <Col
+        key={index}
+        {...xs && { xs: xs }}
+        {...sm && { sm: sm }}
+        {...md && { md: md }}
+        {...lg && { lg: lg }}
+        {...xl && { xl: xl }}
+        className={`py-${paddingY}`}
+      >
+        {onRenderItem(item)}
+      </Col>
+    ))}
+  </Row>
+);
+
+export default MultiColumnList;
