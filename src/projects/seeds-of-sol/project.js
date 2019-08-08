dd151280db70b781d6ef2dcd158a0fa893c8736a
Peter Oberrauner <thealmostbearded@gmail.com>
2019-08-09 00:14:32 +0200

    Change Spelling Mistake

--- src/projects/seeds-of-sol/project.js
+++ src/projects/seeds-of-sol/project.js
@@ -3,7 +3,7 @@ import thumbnail from "./thumbnail.png";
 export default {
   title: "Seeds of Sol",
   tagline:
-    "An twin-stick shooter action roguelike featuring a multi-personality AI companion.",
+    "A twin-stick shooter action roguelike featuring a multi-personality AI companion.",
   thumbnail: thumbnail,
   getContent: () => import("./content.mdx")
 };
