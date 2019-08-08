12fe1d82370cab8a05537b24630652c69029c686
Peter Oberrauner <thealmostbearded@gmail.com>
2019-08-09 00:15:06 +0200

    Add a Period to the Tagline

--- src/projects/battlegrounds/project.js
+++ src/projects/battlegrounds/project.js
@@ -2,7 +2,7 @@ import thumbnail from "./thumbnail.png";
 
 export default {
   title: "paysafecard Battlegrounds",
-  tagline: "A local multiplayer battle royale game based on Pacman",
+  tagline: "A local multiplayer battle royale game based on Pacman.",
   thumbnail: thumbnail,
   getContent: () => import("./content.mdx")
 };
